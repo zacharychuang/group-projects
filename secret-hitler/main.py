c0f4419f4110cbb67b1d64231b4210c70cf3870f
Albert Su <albertsu1+github@berkeley.edu>
2023-06-18 02:34:43 -0700

    lol

--- secret-hitler/main.py
+++ secret-hitler/main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
+
+print("Hello World")
+
+roles = ["Liberal", "Fascist"]
+
+print("Your role is: " + random.choice(roles))
